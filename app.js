4feefb139c1486eff02736f383093ba2eacdb5c5
vibhuti310 <vibhutigupta2002@gmail.com>
2022-11-05 21:42:15 +0530

    Add env vars

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -22,9 +23,9 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-const secret = "ThisIsOurLittleSecrets.";
+
 userSchema.plugin(encrypt, {
-  secret: secret,
+  secret: process.env.SECRET,
   encryptedFields: ["password"]
 });
 
